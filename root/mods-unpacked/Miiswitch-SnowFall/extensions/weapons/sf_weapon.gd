adc0e19073609c78ddabdb220632e06f2f828468
antonin <a9laclau@enib.fr>
2024-11-20 16:46:01 +0100

    snow zone tscn + spawn of snow zone

--- root/mods-unpacked/Miiswitch-SnowFall/extensions/weapons/sf_weapon.gd
+++ root/mods-unpacked/Miiswitch-SnowFall/extensions/weapons/sf_weapon.gd
@@ -1,7 +1,8 @@
 extends "res://weapons/weapon.gd"
 
-
 const SNOWFALL_LOG = "Miiswitch-SnowFall"
+
+var snow_scene = preload("res://mods-unpacked/Miiswitch-SnowFall/content/weapons/ranged/snow_ball/sf_snow_zone.tscn/sf_snow_zone.tscn") # Test scene for the snow scene
 
 # Extensions
 # =============================================================================
@@ -14,6 +15,12 @@ func on_weapon_hit_something(_thing_hit: Node, damage_dealt: int, hitbox: Hitbox
 	
 	for effect in effects:
 		if effect is SFSpawnSnowOnHitEffect and Utils.get_chance_success(effect.chance):
-			ModLoaderLog.info("Effect spawn snow prock", SNOWFALL_LOG)
-	
-
+			ModLoaderLog.info("Effect spawn snow prock", SNOWFALL_LOG) # This works
+			var root_tree = Engine.get_main_loop().get_current_scene()
+			print(root_tree.get_node('Entities'))
+			var snow_zone = snow_scene.instance()
+			snow_zone.position = _thing_hit.global_position
+			root_tree.get_node('Entities').add_child(snow_zone)
+			# var parent_node_weapon = get_tree().get_root() # Doesn't work because weapon is a scene by itself
+			# print(parent_node_weapon.get_parent())		 # I was trying to get the reference of Main.tscn, 
+			# parent_node_weapon.add_child(snow_zone)        # but it doesn't seems to work that way in godot
