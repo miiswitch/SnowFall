f8c4c7abfe330cdac49242a0edca12255a0d46fc
antonin <a9laclau@enib.fr>
2024-11-25 17:05:17 +0100

    remove percentage speed for enemy instead of decaying speed

--- root/mods-unpacked/Miiswitch-SnowFall/content/weapons/ranged/snow_ball/sf_snow_zone.tscn/sf_snow_zone_area_behavior.gd
+++ root/mods-unpacked/Miiswitch-SnowFall/content/weapons/ranged/snow_ball/sf_snow_zone.tscn/sf_snow_zone_area_behavior.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 const SNOWFALL_LOG = "Miiswitch-SnowFall"
+const SPEED_REDUCTION = 0.5 # 50% speed reduction
 
 func _ready():
 	monitoring = true
@@ -9,15 +10,14 @@ func _ready():
 # TODO : Don't change the speed if the target is already affected by a snow zone -> Snow zone effect shouldn't stack
 
 func _on_Area2D_body_entered(body: RigidBody2D):
-	# Change this to decrease the speed of the monster while he is on snow, not decaying
 	if body is Enemy:
-		body.add_decaying_speed( - 300)
+		body.current_stats.speed = body.current_stats.speed * SPEED_REDUCTION
 	# Change this with the player stat
 	if body is Player:
 		body.add_decaying_speed( - 300)
 	
 func _on_Area2D_body_exited(body):
 	if body is Enemy:
-		pass # reset speed of ennemy
+		body.reset_speed_stat(0)
 	if body is Player:
 		pass # reset speed of Player
